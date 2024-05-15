7f5801f5aca6d6209a16fe5b36c1359ec0613a03
Hyeongtak Ji <hyeongtak.ji@gmail.com>
2024-05-15 11:42:13 +0900

    multi-stream: utilize stream ID for host write commands
    LUNs within each channel are divided into streams.  Host writes that
    include a stream identifier are strictly managed; a write command with
    a stream identifier must be processed within its respective stream.
    Consequently, there is no "fallback" stream.  If no free lines are
    available within a stream, subsequent write commands will fail.
    
    Test Example:
      $ printf "hello world" | dd bs=1 count=11 conv=notrunc of=data.bin
      $ dd if=/dev/zero bs=1 count=131072 conv=notrunc of=data.bin oflag=append
      $ nvme io-passthru /dev/nvme0n1 --opcode=0x01 --namespace-id=1 --data-len=16384 --cdw10=0 --cdw11=0 --cdw12=0x1f --cdw13=0 --write --raw-binary < data.bin
    
    Signed-off-by: Hyeongtak Ji <hyeongtak.ji@gmail.com>

--- ssd.c
+++ ssd.c
@@ -150,7 +150,7 @@ void ssd_init_params(struct ssdparams *spp, uint64_t capacity, uint32_t nparts)
 	spp->tt_luns = spp->luns_per_ch * spp->nchs;
 
 	/* line is special, put it at the end */
-	spp->blks_per_line = spp->tt_luns; /* TODO: to fix under multiplanes */
+	spp->blks_per_line = spp->nchs; /* TODO: to fix under multiplanes */
 	spp->pgs_per_line = spp->blks_per_line * spp->pgs_per_blk;
 	spp->secs_per_line = spp->pgs_per_line * spp->secs_per_pg;
 	spp->tt_lines = spp->blks_per_lun;
